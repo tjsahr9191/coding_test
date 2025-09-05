f1883adbcd35a2cbf02191f5fd93755305970fdd
sunmok <rlfhr9191@gmail.com>
2025-09-05 15:49:12 +0900

    feat:improve_bubble_sort

--- week1/sort/bubblesort.py
+++ week1/sort/bubblesort.py
@@ -1,9 +1,40 @@
 inputs = [1, 6, 4, 3, 7, 8, 9]
 
 n = len(inputs)
-for i in range(n-1):
-    for j in range(n-1, i, -1):
-        if inputs[j] < inputs[j-1]:
-            inputs[j], inputs[j-1] = inputs[j-1], inputs[j];
+
+def bubblesort():
+    for i in range(n - 1):
+        for j in range(n - 1, i, -1):
+            if inputs[j] < inputs[j - 1]:
+                inputs[j], inputs[j - 1] = inputs[j - 1], inputs[j]
+
+
+def bubblesort_improve_1():
+    for i in range(n - 1):
+        exchange_count = 0
+        for j in range(n - 1, i, -1):
+            if inputs[j] < inputs[j - 1]:
+                inputs[j], inputs[j - 1] = inputs[j - 1], inputs[j];
+                exchange_count += 1
+
+        if exchange_count == 0:
+            break
+
+
+def bubblesort_improve_2():
+    k = 0
+    while k < n - 1:
+        last = n - 1
+        for j in range(n - 1, k, -1):
+            if inputs[j] < inputs[j - 1]:
+                inputs[j], inputs[j - 1] = inputs[j - 1], inputs[j];
+                last = j
+
+        k = last
+
+
+bubblesort_improve_2()
 
 print(inputs)
+
+
