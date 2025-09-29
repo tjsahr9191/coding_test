707147901beb7dade9638c51696679b0bcb0db91
sunmok <rlfhr9191@gmail.com>
2025-09-29 21:22:09 +0900

    feat:9251

---
+++ week4/9251_LCS/9251.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+a = list(input().strip())
+b = list(input().strip())
+
+dp = [[0] * (len(b)+1) for _ in range(len(a)+1)]
+
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            dp[i+1][j+1] = dp[i][j] + 1
+        else:
+            dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+
+print(dp[len(a)][len(b)])
